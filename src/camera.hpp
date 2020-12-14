22913c67632ccf002f94c86f9c23bac7e75b8bcc
harrisja <jacob.harris443@gmail.com>
2020-12-14 14:34:09 +0000

    added positionable camera

--- src/camera.hpp
+++ src/camera.hpp
@@ -6,7 +6,8 @@
 
 class camera {
   public:
-    camera(double vfov, // vertical fov in degrees
+    camera(point3 lookfrom, point3 lookat, vec3 vup,
+           double vfov, // vertical fov in degrees
            double aspect_ratio) {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
@@ -14,11 +15,15 @@ class camera {
         auto viewport_width = aspect_ratio * viewport_height;
         auto focal_length = 1.0;
 
-        origin = point3(0, 0, 0);
-        horiz = vec3(viewport_width, 0.0, 0.0);
+        auto w = unit_vector(lookfrom - lookat);
+        auto u = unit_vector(cross(vup, w));
+        auto v = cross(w, u);
+
+        origin = lookfrom;
+        horiz = viewport_width * u;
+        vert = viewport_height * v;
         vert = vec3(0.0, viewport_height, 0.0);
-        lower_left_corner =
-            origin - horiz / 2 - vert / 2 - vec3(0, 0, focal_length);
+        lower_left_corner = origin - horiz / 2 - vert / 2 - w;
     }
 
     ray get_ray(double u, double v) const {
